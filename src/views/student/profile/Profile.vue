9e8b5568a5cb2eac7fed316855bfd9787be92da4
lovestories0eng <1494121350@qq.com>
2022-02-03 22:51:34 +0800

    add Profile component

--- src/views/student/profile/Profile.vue
+++ src/views/student/profile/Profile.vue
@@ -1,5 +1,49 @@
 <template>
-  <div></div>
+  <div>
+    <el-card>
+      <el-descriptions class="margin-top" title="用户信息" :column="3" :size="size" border>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-user"></i>
+            用户名
+          </template>
+          {{ $store.getters.name }}
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-mobile-phone"></i>
+            手机号
+          </template>
+          18100000000
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-location-outline"></i>
+            居住地
+          </template>
+          苏州市
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-tickets"></i>
+            备注
+          </template>
+          <el-tag size="small">学校</el-tag>
+        </el-descriptions-item>
+        <el-descriptions-item>
+          <template slot="label">
+            <i class="el-icon-office-building"></i>
+            联系地址
+          </template>
+          江苏省苏州市吴中区吴中大道 1188 号
+        </el-descriptions-item>
+      </el-descriptions>
+    </el-card>
+    <div style="text-align: center; display: block">
+      <div style="font-size: 18px; font-weight: 700;">用户头像</div>
+      <el-avatar :size="800" src="https://lovestories0eng.github.io/cdn/avatar.jpg"></el-avatar>
+    </div>
+  </div>
 </template>
 
 <script>
