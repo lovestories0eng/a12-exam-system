bdb600e90e4603548050dd6be1933053eef41f09
lovestories0eng <1494121350@qq.com>
2022-02-04 23:23:06 +0800

    start notice component

--- src/views/student/notice/Received.vue
+++ src/views/student/notice/Received.vue
@@ -1,5 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark">
+          考试通知
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple-dark">
+          成绩推送
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -9,5 +24,18 @@ export default {
 </script>
 
 <style scoped>
+.bg-purple-dark {
+  background: #99a9bf;
+}
+
+.grid-content {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: center;
+  margin: 25px;
+  border-radius: 4px;
+  min-height: 36px;
+}
 
 </style>
